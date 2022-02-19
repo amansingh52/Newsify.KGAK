47e0617a287fe7e6e9b754461b4815b7294e400e
amansingh52 <mca2021.amansingh@asmedu.org>
2022-02-19 16:13:48 +0530

    first commit

---
+++ app/src/main/java/com/maverick/newsify/Login.java
@@ -0,0 +1,52 @@
+package com.maverick.newsify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+    }
+    public void MainActivity(View view){
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+
+        Pair[] pairs = new Pair[1];
+
+        pairs[0] = new Pair<View, String>(findViewById(R.id.login_btn),"transition_login");
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this, pairs);
+            startActivity(intent, options.toBundle());
+        }
+        else {
+
+            startActivity(intent);
+        }
+    }
+    public void SignUp(View view){
+        Intent intent = new Intent(getApplicationContext(),SignUp.class);
+
+        Pair[] pairs = new Pair[1];
+
+        pairs[0] = new Pair<View, String>(findViewById(R.id.login_btn),"transition_login");
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this, pairs);
+            startActivity(intent, options.toBundle());
+        }
+        else {
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/maverick/newsify/SignUp.java
@@ -0,0 +1,120 @@
+package com.maverick.newsify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class SignUp extends AppCompatActivity {
+
+    //Variables
+
+    ImageView backBtn;
+    Button next, login;
+    TextView titleText;
+
+    TextInputLayout fullName, username, email, password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_sign_up);
+
+        //Hooks
+        backBtn = findViewById(R.id.signup_back_button);
+        next = findViewById(R.id.signup_next_button);
+        login = findViewById(R.id.signup_login_button);
+        titleText = findViewById(R.id.signup_title_text);
+
+        fullName = findViewById(R.id.signup_fullname);
+        username = findViewById(R.id.signup_username);
+        email = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_password);
+
+    }
+
+    public void callNextSignupScreen(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), SignUp2ndClass.class);
+
+        //Add transition
+        Pair[] pairs = new Pair[4];
+
+        if(!validateFullName() | !validateUserName() | !validateEmail()){
+            return;
+        }
+
+        pairs[0] = new Pair<View, String>(backBtn, "transition_back_arrow_btn");
+        pairs[1] = new Pair<View, String>(next, "transition_next_btn");
+        pairs[2] = new Pair<View, String>(login, "transition_login_btn");
+        pairs[3] = new Pair<View, String>(titleText, "transition_title_text");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp.this, pairs);
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
+    }
+
+    // Validation Functions
+    private boolean validateFullName() {
+        String val = fullName.getEditText().getText().toString().trim();
+        if (val.isEmpty()) {
+            fullName.setError("Field can not be empty");
+            return false;
+        } else {
+            fullName.setError(null);
+            fullName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateUserName() {
+        String val = username.getEditText().getText().toString().trim();
+        String checkspaces = "Aw{1,20}z";
+
+        if (val.isEmpty()) {
+            username.setError("Field can not be empty");
+            return false;
+        } else if (val.length() > 20) {
+            username.setError("Username is too large!");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        String val = email.getEditText().getText().toString().trim();
+        String checkEmail = "[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+";
+
+        if (val.isEmpty()) {
+            email.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkEmail)) {
+            email.setError("Invalid Email!");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+}
