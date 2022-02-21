83eca0c973158dba7faf824046094071f2bbd63b
amansingh52 <mca2021.amansingh@asmedu.org>
2022-02-22 03:28:45 +0530

    User Dashboard  4A design

---
+++ app/src/main/java/com/maverick/newsify/Home.java
@@ -0,0 +1,39 @@
+package com.maverick.newsify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class Home extends AppCompatActivity {
+    private ArrayList<CategoryRVModal> categoryRVModalArrayList;
+    private CategoryRVAdapter categoryRVAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+    public void MainActivity(View view){
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+
+        Pair[] pairs = new Pair[1];
+
+        pairs[0] = new Pair<View, String>(findViewById(R.id.home_btn),"transition_login");
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Home.this, pairs);
+            startActivity(intent, options.toBundle());
+        }
+        else {
+            startActivity(intent);
+        }
+    }
+}
